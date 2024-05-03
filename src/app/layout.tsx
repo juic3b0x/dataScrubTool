b236e5b753e0d78b63f3c1c8c7831bb0dbb95d30
juic3b0x <70825623+juic3b0x@users.noreply.github.com>
2024-05-03 02:50:02 -0500

    fix: fixed SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,6 @@ export default function RootLayout({
                 </footer>
                 <SpeedInsights />
                 <Analytics />
-                <SpeedInsights />
             </body>
         </html>
     );
