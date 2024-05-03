302813f31ba679074856914fde86a91ba323c606
juic3b0x <70825623+juic3b0x@users.noreply.github.com>
2024-05-03 02:48:38 -0500

    fix: fixed SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -32,6 +33,7 @@ export default function RootLayout({
                         trademarks are the property of their respective owners.
                     </p>
                 </footer>
+                <SpeedInsights />
                 <Analytics />
             </body>
         </html>
